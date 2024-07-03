2fe173aaa69f85d79d2d02fe5eb5886df771d93f
Ad-Car <49081037+Ad-Car@users.noreply.github.com>
2024-07-03 22:51:02 +0100

    Implement pen effect

--- script.js
+++ script.js
@@ -3,17 +3,21 @@ const container = document.querySelector("#container");
 //const container2 = document.createElement("div");
 const body = document.querySelector("body");
 
+function changeBackground(e) {
+	console.log(e)
+}
 
-let x
-for (x = 0; x < 16; x++) {
+let i
+for (i = 0; i < 16; i++) {
 const row = document.createElement("div");
 row.classList.add("row");
 container.appendChild(row);
 
-let i 
-for (i = 0; i < 16; i ++) {
+let j 
+for (j = 0; j < 16; j ++) {
 const pixel = document.createElement("span");
 pixel.classList.add("pixel"); 
+pixel.addEventListener("mouseover", ()=> {pixel.classList.add("dark-pixel");});
 row.appendChild(pixel);
 }
 }
